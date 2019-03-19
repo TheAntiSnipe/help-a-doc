bf28f167a49d0397746dda2f314ab1d7c21279a7
Unknown <pranavgharat1999@gmail.com>
2019-03-19 20:00:48 +0530

    added code that creates csv file

--- test2.py
+++ test2.py
@@ -31,27 +31,20 @@ df8 = pd.read_sql_query(query, con)
 
 with open('C:/Users/Pranav/Desktop/Code/projects/help-a-doc/help-a-doc/symptoms.json') as f:
     j_son = json.load(f)
-data2=[]
-final_index = []
-for x in range(10):
-    list1=[]
-    indices = []
-    text = str(df8.values[x]).lower().split(" ")
-    for j in text:
-        if j in j_son:
-            list1.append(j)
-            indices.append(j_son.index(j))
-    data1 = list(set(list1))
-    symptom_list_index = sorted(list(set(indices)))
-    data2.append(data1)
-    csv_list = []
-    for con in range(0,540):            #Finding whether symptoms are present or not
-        if con in symptom_list_index:   #If present append 1
-            csv_list.append(1)
-        else:                           #If not then append 0
-            csv_list.append(0)
-    final_index.append(csv_list)        #Final symptom list that has either 0 or 1
-    data = {'Symptoms':data2}
 
-df = pd.DataFrame(data)
-df
+with open('symptoms_list.csv','a') as f:
+    f.write("symptom1,symptom2,symptom3,symptom4,symptom5,symptom6,symptom7,symptom8,symptom9,symptom10,symptom11,symptom12,symptom13,symptom14,symptom15\n")
+    for x in range(10):
+        list1=[]
+        text = str(df8.values[x]).lower().split(" ")
+        for j in text:
+            if j in j_son:
+                list1.append(j)
+        data1 = sorted(list(set(list1)))
+        if len(data1) == 0:
+            continue
+        elif len(data1) < 15:
+            for con in range(len(data1),16):
+                data1.append('NaN')
+        f.write(",".join(data1))
+        f.write("\n")
